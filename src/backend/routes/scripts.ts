e5559261238d6679137206537fa6605ceb5c0f51
fate <66224951+fatesc@users.noreply.github.com>
2021-04-28 20:18:11 +0100

    change address

--- src/backend/routes/scripts.ts
+++ src/backend/routes/scripts.ts
@@ -72,7 +72,7 @@ const RateLimited: Map<WebSocket, number> = new Map();
 const RateLimitTime = .5
 
 export function websocketCon(client: WebSocket, request: IncomingMessage) {
-	const userip = request.socket.localAddress
+	const userip = <string>request?.headers?.["X-Forwarded-For"] ?? request?.socket?.localAddress
 	if (ConnectedUsers.includes(userip)) return client.close();
 	ConnectedUsers.push(userip);
 	
