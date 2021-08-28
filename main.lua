8d26a80a109fc33cb8013a9c3d40d2501796d535
stan <51496538+stan2474@users.noreply.github.com>
2021-08-28 01:06:52 -0400

    kill+kill2 fix + loopkill fix

--- main.lua
+++ main.lua
@@ -2016,7 +2016,7 @@ AddCommand("kill", {"tkill"}, "kills someone", {"1", 1, 3}, function(Caller, Arg
             end
         end
     end)()
-    Character.HumanoidRootPart.CFrame = CFrame.new(9e9,5e5,9e9)
+    Character.HumanoidRootPart.CFrame = CFrame.new(99,workspace.FallenPartsDestroyHeight+50,0)
     wait(.3);
     Destroy(Character);
     Character = CWait(LocalPlayer.CharacterAdded);
@@ -2082,7 +2082,7 @@ AddCommand("kill2", {}, "another variant of kill", {1, "1"}, function(Caller, Ar
             end
         end
     end)()
-    GetCharacter().HumanoidRootPart.CFrame = CFrame.new(9e9,5e5,9e9)
+    GetCharacter().HumanoidRootPart.CFrame = CFrame.new(99,workspace.FallenPartsDestroyHeight+50,0)
     if (Destroy_) then
         wait(1);
         ReplaceCharacter();
@@ -2101,7 +2101,7 @@ AddCommand("loopkill", {}, "loopkill loopkills a character", {3,"1"}, function(C
         UnequipTools(Humanoid);
         DisableAnimate();
         Humanoid = ReplaceHumanoid(Humanoid);
-        ChangeState(Humanoid, 15);
+       Character.HumanoidRootPart.CFrame = CFrame.new(99,workspace.FallenPartsDestroyHeight+50,0)
         if (isR6(Target[1])) then
             Utils.Notify(LocalPlayer, "Loopkill", "the player is in r6 it will only kill every 2 respawns")
         end
