7b5bf1e960b5902e37d982b8aaeaaa35840f2ea7
NotDSF <67711164+NotDSF@users.noreply.github.com>
2021-10-26 22:01:38 +0100

    Added Themes 2
    More info on docs

--- main.lua
+++ main.lua
@@ -1207,16 +1207,103 @@ end
 UI = Clone(game.GetObjects(game, "rbxassetid://6167929302")[1]);
 UI.Enabled = true
 
-if isfolder("fates-admin") and not isfile("fates-admin/UI.json") then writefile("fates-admin/UI.json", game:HttpGet("https://pastebin.com/raw/JUZCZeBF")) end;
-
-local UIConfig;
 local CommandBarPrefix;
+local UIConfig = {
+    CommandBar = {
+        Background = {
+            Color = {
+                R = 32,
+                G = 33,
+                B = 36
+            },
+            Transparency = 0.25
+        },
+        InputText = {
+            Color = {
+                R = 255,
+                G = 255,
+                B = 255
+            }
+        },
+        Arrow = {
+            Color = {
+                R = 255,
+                G = 255,
+                B = 255
+            }
+        }
+    },
+    CommandList = {
+        Background = {
+            Color = {
+                R = 32,
+                G = 33,
+                B = 36
+            },
+            Transparency = 0.25
+        },
+        Title = {
+            Text = "Search Commands",
+            Color = {
+                R = 184,
+                G = 187,
+                B = 195
+            }
+        }
+    },
+    Notification = {
+        Background = {
+            Color = {
+                R = 32,
+                G = 33,
+                B = 36
+            },
+            Transparency = 0.25
+        },
+        Title = {
+            Text = "Notification",
+            Color = {
+                R = 220,
+                G = 224,
+                B = 234
+            }
+        },
+        Message = {
+            Color = {
+                R = 220,
+                G = 224,
+                B = 234
+            }
+        }
+    },
+    Command = {
+        Background = {
+            Color = {
+                R = 32,
+                G = 33,
+                B = 36
+            },
+            Transparency = 0.75
+        },
+        TextColor = {
+            R = 220,
+            G = 224,
+            B = 234
+        }
+    }
+}
+
+if isfolder("fates-admin") and not isfile("fates-admin/UI.json") then 
+    local Data = JSONEncode(Services.HttpService, UIConfig);
+    writefile("fates-admin/UI.json", Data);
+    writefile("fates-admin/UI-Backup.json", Data);
+end;
 
 do
-    local ok, res = pcall(game.HttpService.JSONDecode, game.HttpService, readfile("fates-admin/UI.json"));
+    local ok, res = pcall(JSONDecode, Services.HttpService, readfile("fates-admin/UI.json"));
     local Config = GetConfig();
 
-    UIConfig = ok and res or game.HttpService:JSONDecode(game:HttpGet("https://pastebin.com/raw/JUZCZeBF"));
+    UIConfig = ok and res or UIConfig;
     CommandBarPrefix = isfolder and (Config.CommandBarPrefix and Enum.KeyCode[Config.CommandBarPrefix] or Enum.KeyCode.Semicolon) or Enum.KeyCode.Semicolon
 end
 
@@ -1262,17 +1349,36 @@ ProtectInstance(Commands.Search, true);
 
 -- Loading theme
 do
-    local CommandBarB = UIConfig.CommandBar.Background
-    CommandBar.BackgroundColor3 = Color3.fromRGB(CommandBarB.Color.R, CommandBarB.Color.G, CommandBarB.Color.B);
-    CommandBar.BackgroundTransparency = CommandBarB.Transparency;
+    local fromRGB = Color3.fromRGB;
+    local CloneCmdC = UIConfig.Command; 
+    local CommandBarC = UIConfig.CommandBar;
+    local CommandListC = UIConfig.CommandList;
+    local NotificationC = UIConfig.Notification;
 
-    local CommandListB = UIConfig.CommandList.Background
-    Commands.BackgroundColor3 = Color3.fromRGB(CommandListB.Color.R, CommandListB.Color.G, CommandListB.Color.B);
-    Commands.BackgroundTransparency = CommandListB.Transparency;
+    CommandBar.BackgroundColor3 = fromRGB(CommandBarC.Background.Color.R, CommandBarC.Background.Color.G, CommandBarC.Background.Color.B);
+    CommandBar.BackgroundTransparency = CommandBarC.Background.Transparency;
 
-    local NotificationB = UIConfig.Notification.Background
-    Notification.BackgroundColor3 = Color3.fromRGB(NotificationB.Color.R, NotificationB.Color.G, NotificationB.Color.B);
-    Notification.BackgroundTransparency = NotificationB.Transparency;
+    CommandBar.Input.TextColor3 = fromRGB(CommandBarC.InputText.Color.R, CommandBarC.InputText.Color.G, CommandBarC.InputText.Color.B);
+    CommandBar.Arrow.TextColor3 = fromRGB(CommandBarC.Arrow.Color.R, CommandBarC.Arrow.Color.G, CommandBarC.Arrow.Color.B);
+
+    Commands.BackgroundColor3 = fromRGB(CommandListC.Background.Color.R, CommandListC.Background.Color.G, CommandListC.Background.Color.B);
+    Commands.BackgroundTransparency = CommandListC.Background.Transparency;
+
+    Commands.Search.PlaceholderColor3 = fromRGB(CommandListC.Title.Color.R, CommandListC.Title.Color.G, CommandListC.Title.Color.B);
+    Commands.Search.PlaceholderText = CommandListC.Title.Text;
+
+    Notification.BackgroundColor3 = fromRGB(NotificationC.Background.Color.R, NotificationC.Background.Color.G, NotificationC.Background.Color.B);
+    Notification.BackgroundTransparency = NotificationC.Background.Transparency;
+
+    Notification.Title.TextColor3 = fromRGB(NotificationC.Title.Color.R, NotificationC.Title.Color.G, NotificationC.Title.Color.B);
+    Notification.Title.Text = NotificationC.Title.Text;
+
+    Notification.Message.TextColor3 = fromRGB(NotificationC.Message.Color.R, NotificationC.Message.Color.G, NotificationC.Message.Color.B);
+
+    Command.CommandText.TextColor3 = fromRGB(CloneCmdC.TextColor.R, CloneCmdC.TextColor.G, CloneCmdC.TextColor.B);
+
+    Command.BackgroundColor3 = fromRGB(CloneCmdC.Background.Color.R, CloneCmdC.Background.Color.G, CloneCmdC.Background.Color.B);
+    Command.BackgroundTransparency = CloneCmdC.Background.Transparency;
 end;
 --END IMPORT [ui]
 
